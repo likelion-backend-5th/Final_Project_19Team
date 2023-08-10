2a77b40a1d363fa983649cf2f8b2d66b18fe7619
moonwonki <moonwonki1999@gmail.com>
2023-08-10 17:26:13 +0900

    Init. Initial Commit

---
+++ src/main/java/com/likelion/teammatch/TeamMatchApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.teammatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/teammatch/TeamMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.teammatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
