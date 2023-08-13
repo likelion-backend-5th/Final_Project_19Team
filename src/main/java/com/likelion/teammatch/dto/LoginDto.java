dc679e5fa023d819e95ce50ef6beca05c34363d9
moonwonki <moonwonki1999@gmail.com>
2023-08-13 18:16:59 +0900

    feat: 인증 단계에서 필요한 Dto 추가
    - token 생성 결과를 응답하는 데 사용되는 JwtTokenDto
    - 유저네임과 패스워드 입력을 받아오는 LoginDto
    - 회원가입 시 필수적으로 받아야 하는 정보를 받아오는 RegisterDto

---
+++ src/main/java/com/likelion/teammatch/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.likelion.teammatch.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDto {
+    private String username;
+    private String password;
+}
