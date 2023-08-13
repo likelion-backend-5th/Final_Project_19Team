0ebb925cc947f0a07f92b5ed8c6e7cb4f2c0d1e6
moonwonki <moonwonki1999@gmail.com>
2023-08-13 18:14:41 +0900

    feat: User 엔티티와 UserRepository 추가
    - UserDetails를 상속받은 User 엔티티 추가
    - jpaRepository 상속받은 UserRepository 추가
    - UserRepository에 findByUsername, existsBy(유일 속성 컬럼 3개) 메소드 추가

---
+++ src/main/java/com/likelion/teammatch/entity/User.java
@@ -0,0 +1,76 @@
+package com.likelion.teammatch.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PACKAGE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+    @Column(columnDefinition = "TEXT")
+    private String introduction;
+
+    @Column(columnDefinition = "VARCHAR(225)")
+    private String location;
+
+    private Integer reported;
+
+    @Column(columnDefinition = "TEXT")
+    private String prize;
+
+    @Column(columnDefinition = "TEXT")
+    private String past;
+
+    @Column(columnDefinition = "VARCHAR(225)")
+    private String github;
+
+    private String ServerRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/likelion/teammatch/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.likelion.teammatch.repository;
+
+import com.likelion.teammatch.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByPhone(String phone);
+}
