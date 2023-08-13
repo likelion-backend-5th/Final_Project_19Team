7f8d4eca6fc8a1c98ad513335537fde7696c391b
moonwonki <moonwonki1999@gmail.com>
2023-08-13 19:26:08 +0900

    feat: TechStack, UserTechStack 엔티티와 Repository 추가
    - TechStack 엔티티 추가 (추후에 수정할 예정)
    - TechStack과 User의 중간 테이블인 UserTechStack 엔티티 추가
    - UserTechStackRepository 추가
    - TechStackRepository 추가
    - 각 레포지토리 인터페이스에 추후 사용할 것으로 보이는 메소드 추가

---
+++ src/main/java/com/likelion/teammatch/repository/TechStackRepository.java
@@ -0,0 +1,12 @@
+package com.likelion.teammatch.repository;
+
+import com.likelion.teammatch.entity.TechStack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TechStackRepository extends JpaRepository<TechStack, Long> {
+
+    //기술 이름으로 TechStack을 찾도록 하는 메소드
+    Optional<TechStack> findByName(String name);
+}
