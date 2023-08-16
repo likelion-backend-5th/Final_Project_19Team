6daa7ef36991932a9edb4a456dd7d3badd8bcffd
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-08-16 18:37:19 +0900

    feat: User 엔티티, 리포지토리 수정 - 엔티티에 Team 필드 추가 - 리포지토리에 findUserNameByTeam 추가

--- src/main/java/com/likelion/teammatch/entity/User.java
+++ src/main/java/com/likelion/teammatch/entity/User.java
@@ -73,4 +73,15 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 팀 하나에 유저 여러명
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    private Integer adminId;
+    private String nickname;
+
+    public void joinTeam(Team team) {
+        this.team = team;
+    }
 }
