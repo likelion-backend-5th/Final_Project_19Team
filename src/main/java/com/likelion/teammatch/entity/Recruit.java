d59dc12fcaf671853f6213a6075aa06ea35d3e17
moonwonki <moonwonki1999@gmail.com>
2023-08-24 10:28:12 +0900

    feat. Recruit 엔티티와 레포지토리 추가
    - Team 엔티티에 포함되지 않은 정보들을 Recruit에 넣어둠
    - 추후 Recruit 정보를 가져올 DTO 만들 계획
    - Repository는 Page로 가장 최신 Recruit을 가져올 수 있도록 메소드 추가

---
+++ src/main/java/com/likelion/teammatch/entity/Recruit.java
@@ -0,0 +1,30 @@
+package com.likelion.teammatch.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Recruit extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PACKAGE)
+    private Long id;
+
+    private Long teamManagerId;
+    private Long teamId;
+    
+    //모집 공고 제목
+    private String title;
+    //모집 공고 추가 소개글
+    private String teamRecruitDetails;
+    //모집 목표 인원수
+    private Integer recruitMemberNum;
+    //badge 관련 사항은 미구현
+//    private Boolean badge;
+}
---
+++ src/main/java/com/likelion/teammatch/repository/RecruitRepository.java
@@ -0,0 +1,10 @@
+package com.likelion.teammatch.repository;
+
+import com.likelion.teammatch.entity.Recruit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+    Page<Recruit> findAllByIdOrderByIdDesc(PageRequest pageReqeust);
+}
