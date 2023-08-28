562e9bc0a6dcc05ef1feb0fa873e36fe31f1e32b
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-08-28 11:47:24 +0900

    Fix: 스프링 실행 오류 문제 해결 - CommentRepository 오타 수정 (RecruitId -> ProjectResultId)

--- src/main/java/com/likelion/teammatch/repository/CommentRepository.java
+++ src/main/java/com/likelion/teammatch/repository/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Service
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByRecruitId(Long id);
+    List<Comment> findAllByProjectResultId(Long id);
 }
