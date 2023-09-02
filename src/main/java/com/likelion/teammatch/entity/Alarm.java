63fbc75c7a8706cbbf0274c18719126412536dd2
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-09-03 02:07:15 +0900

    feat. Alarm Entity 수정 - 필드 수정 - receivers 필드는 여러 수신자 User 를 가짐

--- src/main/java/com/likelion/teammatch/entity/Alarm.java
+++ src/main/java/com/likelion/teammatch/entity/Alarm.java
@@ -1,11 +1,11 @@
 package com.likelion.teammatch.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,53 +14,14 @@ public class Alarm extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // 알람 유형
-    private String alarmType;
-    // 프로젝트 명
-    private String projectName;
-    // 알림 조회 여부
-    private Boolean status;
-    // 팀장 or 팀원
-    private String memberType;
-    private Long userId;
-    // 수락, 거절, 탈퇴하기 등
-    private String action;
-
-    // 생성자
-    protected Alarm() {
-        // JPA 사용을 위해 기본 생성자 필요
-}
-    private Alarm(String alarmType, String projectName, String memberType, Long userId) {
-        this.alarmType = alarmType;
-        this.projectName = projectName;
-        this.memberType = memberType;
-        this.userId = userId;
-        this.status = false; // 초기값 설정
-        this.action = ""; // 초기값 설정
-    }
-
-    // 알람 상태 업데이트 메소드
-    public void markAsRead() {
-        this.status = true;
-    }
-
-    // 알람 액션 처리 메소드
-    public void performAction(String action) {
-        // 액션 처리 로직 분기
-        if ("accept".equalsIgnoreCase(action)) {
-            // 수락 처리 로직
-        } else if ("reject".equalsIgnoreCase(action)) {
-            // 거절 처리 로직
-        } else if ("leave".equalsIgnoreCase(action)) {
-            // 탈퇴 처리 로직
-        } else {
-            throw new IllegalArgumentException("Invalid action");
-        }
-        this.action = action;
-    }
-
-    // 정적 팩토리 메소드
-    public static Alarm create(String alarmType, String projectName, String memberType, Long userId) {
-        return new Alarm(alarmType, projectName, memberType, userId);
-    }
+    // 내용
+    private String text;
+    // 수신자
+    @ManyToMany
+    @JoinTable(
+            name = "alarm_receiver",
+            joinColumns = @JoinColumn(name = "alarm_id"),
+            inverseJoinColumns = @JoinColumn(name = "receiver_id")
+    )
+    private List<User> receivers = new ArrayList<>();
 }
