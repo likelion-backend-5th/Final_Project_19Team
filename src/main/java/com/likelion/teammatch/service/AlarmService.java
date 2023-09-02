66153f109f37f4c9fd19ea3ead49b87f96f0ae7a
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-09-03 02:15:08 +0900

    feat. AlarmService 수정 - createAndCallAlarm 삭제 - getAllAlarmsByUserId 삭제 - markAlarmAsRead 삭제 - performAlarmAction 삭제 - createAlarm 추가 - createTeamAlarm 추가 - deleteAlarmForUser 추가 - getAlarmsForUser 추가

--- src/main/java/com/likelion/teammatch/service/AlarmService.java
+++ src/main/java/com/likelion/teammatch/service/AlarmService.java
@@ -1,42 +1,94 @@
 package com.likelion.teammatch.service;
 
 import com.likelion.teammatch.entity.Alarm;
+import com.likelion.teammatch.entity.Team;
+import com.likelion.teammatch.entity.User;
 import com.likelion.teammatch.repository.AlarmRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.likelion.teammatch.repository.UserRepository;
+import com.likelion.teammatch.repository.team.TeamRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AlarmService {
-    @Autowired
-    private AlarmRepository alarmRepository;
+    private final AlarmRepository alarmRepository;
+    private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
 
-    public void createAndCallAlarm(String alarmType, String projectName, String memberType, Long userId, String action) {
-        Alarm alarm = Alarm.create(alarmType, projectName, memberType, userId);
+    public AlarmService(AlarmRepository alarmRepository, TeamRepository teamRepository, UserRepository userRepository) {
+        this.alarmRepository = alarmRepository;
+        this.teamRepository = teamRepository;
+        this.userRepository = userRepository;
+    }
+
+    // 알람 생성 및 수신자 추가
+    public void createAlarm(String text, List<Long> receiverIdList) {
+        Alarm alarm = new Alarm();
+        alarm.setText(text);
+
+        // receiverIdList에 있는 사용자들을 찾아서 receivers에 추가
+        for (Long receiverId : receiverIdList) {
+            User receiver = userRepository.findById(receiverId).orElse(null);
+            if (receiver != null) {
+                alarm.getReceivers().add(receiver);
+            }
+        }
+
+        // 알람 저장
         alarmRepository.save(alarm);
     }
 
-    public List<Alarm> getAllAlarmsByUserId(Long userId) {
-        return alarmRepository.findByUserId(userId);
-    }
+    // 알람 생성 및 수신자 추가
+    public void createTeamAlarm(String text, Long teamId, String currentUsername) {
+        // username을 기반으로 해당 사용자의 ID를 데이터베이스에서 찾아온다고 가정
+        Optional<User> currentUserOptional = userRepository.findByUsername(currentUsername);
 
-    public void markAlarmAsRead(Long alarmId) {
-        Optional<Alarm> alarmOptional = alarmRepository.findById(alarmId);
-        if (alarmOptional.isPresent()) {
-            Alarm alarm = alarmOptional.get();
-            alarm.markAsRead();
+        if (currentUserOptional.isPresent()) {
+            User currentUser = currentUserOptional.get();
+
+            // 팀을 가져옴
+            Team team = teamRepository.findById(teamId)
+                    .orElseThrow(() -> new IllegalArgumentException("Team not found with ID: " + teamId));
+
+            // 팀 멤버 중 본인을 제외한 모든 멤버를 수신자로 추가
+            List<User> receivers = team.getMembers().stream()
+                    .filter(member -> !member.getId().equals(currentUsername))
+                    .collect(Collectors.toList());
+
+            // 알람 생성 및 수신자 설정
+            Alarm alarm = new Alarm();
+            alarm.setText(text);
+            alarm.setReceivers(receivers);
+
+            // 알람 저장
             alarmRepository.save(alarm);
         }
     }
 
-    public void performAlarmAction(Long alarmId, String action) {
-        Optional<Alarm> alarmOptional = alarmRepository.findById(alarmId);
-        if (alarmOptional.isPresent()) {
-            Alarm alarm = alarmOptional.get();
-            alarm.performAction(action);
-            alarmRepository.save(alarm);
+    // 특정 사용자가 자신에게 전송된 알람을 제거
+    public void deleteAlarmForUser(Long alarmId, String username) {
+        Optional<Alarm> optionalAlarm = alarmRepository.findById(alarmId);
+        if (optionalAlarm.isPresent()) {
+            Alarm alarm = optionalAlarm.get();
+
+            // 해당 알람이 사용자에게 전송되었는지 확인
+            if (alarm.getReceivers().stream().anyMatch(user -> user.getUsername().equals(username))) {
+                alarmRepository.deleteById(alarmId);
+            } else {
+                // 해당 알람이 사용자에게 전송되지 않은 경우 예외 처리
+                throw new IllegalArgumentException("The alarm is not for the specified user.");
+            }
+        } else {
+            // 해당 알람이 존재하지 않는 경우 예외 처리
+            throw new IllegalArgumentException("Alarm not found with ID: " + alarmId);
         }
+    }
+
+    // 특정 사용자에게 전송된 알람 목록을 가져오는 메서드
+    public List<Alarm> getAlarmsForUser(String username) {
+        return alarmRepository.findByReceiversUsername(username);
     }
 }
