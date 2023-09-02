19bfcfe8f50f3f8212e50c917060f51662a15ebe
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-09-03 02:08:11 +0900

    feat. Alarm Repository 수정 - findByReceiversUsername 메소드 추가

--- src/main/java/com/likelion/teammatch/repository/AlarmRepository.java
+++ src/main/java/com/likelion/teammatch/repository/AlarmRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-    List<Alarm> findByUserId(Long userId);
+    List<Alarm> findByReceiversUsername(String username);
 }
