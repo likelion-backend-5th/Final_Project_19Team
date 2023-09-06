4d8912ad1ccaaad2cd17de303c85e828032f780b
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-09-06 10:28:32 +0900

    feat. TeamRepository 수정 - findByIdAndDeletedFalse 메소드 추가

--- src/main/java/com/likelion/teammatch/repository/team/TeamRepository.java
+++ src/main/java/com/likelion/teammatch/repository/team/TeamRepository.java
@@ -3,4 +3,8 @@ package com.likelion.teammatch.repository.team;
 import com.likelion.teammatch.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {}
+import java.util.Optional;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByIdAndDeletedFalse(Long id);
+}
