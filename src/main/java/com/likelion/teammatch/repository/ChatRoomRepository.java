e5dd006befada4160a8accafb14ace055642c569
moonwonki <moonwonki1999@gmail.com>
2023-09-07 05:03:01 +0900

    feat. 팀 서비스 변경에 따른 Repository 와 dto 변경

--- src/main/java/com/likelion/teammatch/repository/ChatRoomRepository.java
+++ src/main/java/com/likelion/teammatch/repository/ChatRoomRepository.java
@@ -1,9 +1,11 @@
 package com.likelion.teammatch.repository;
 
 import com.likelion.teammatch.entity.ChatRoom;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     // TeamId에 해당하는 모든 채팅방 삭제
+    @Transactional
     void deleteAllByTeamId(Long teamId);
 }
