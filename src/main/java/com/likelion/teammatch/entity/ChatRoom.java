98bdf96087d218ee691b7a9cd319366f7b406e97
Leesiyun <livre303@gmail.com>
2023-09-07 11:44:55 +0900

    feat: 채팅 관련 entity 변경

--- src/main/java/com/likelion/teammatch/entity/ChatRoom.java
+++ src/main/java/com/likelion/teammatch/entity/ChatRoom.java
@@ -1,11 +1,15 @@
 package com.likelion.teammatch.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "chat_room")
-@Data
+@Setter
+@Getter
+@ToString
 public class ChatRoom extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
