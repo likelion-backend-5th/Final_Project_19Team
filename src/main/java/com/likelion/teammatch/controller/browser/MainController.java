a8fac098c398b221641ecf799f3c04e1f804adac
Leesiyun <livre303@gmail.com>
2023-09-08 14:10:52 +0900

    feat: 프로필창에 연락처, 이메일 정보 표시

--- src/main/java/com/likelion/teammatch/controller/browser/MainController.java
+++ src/main/java/com/likelion/teammatch/controller/browser/MainController.java
@@ -1,7 +1,9 @@
 package com.likelion.teammatch.controller.browser;
 
 import com.likelion.teammatch.dto.RecruitDraftDto;
+import com.likelion.teammatch.dto.UserProfileDto;
 import com.likelion.teammatch.service.RecruitService;
+import com.likelion.teammatch.service.UserService;
 import com.likelion.teammatch.service.team.TeamService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,20 +19,28 @@ public class MainController {
 
     private final TeamService teamService;
     private final RecruitService recruitService;
+    private final UserService userService;
 
-    public MainController(TeamService teamService, RecruitService recruitService) {
+    public MainController(TeamService teamService, RecruitService recruitService, UserService userService) {
         this.teamService = teamService;
         this.recruitService = recruitService;
+        this.userService = userService;
     }
 
     @GetMapping("/main")
     public String getMainPage(
-            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page, UserProfileDto dto,
             Model model
     ){
         List<RecruitDraftDto> recruitInfoList = recruitService.getRecruitDraftList(page);
         model.addAttribute("recruitList", recruitInfoList);
-        model.addAttribute("username", SecurityContextHolder.getContext().getAuthentication().getName());
+
+        // 프로필창 정보
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserProfileDto profileOfUser = userService.getProfileOfUser(username);
+        model.addAttribute("username", username);
+        model.addAttribute("phone", profileOfUser.getPhone());
+        model.addAttribute("email", profileOfUser.getEmail());
         if (SecurityContextHolder.getContext().getAuthentication().getName().equals("anonymousUser")) model.addAttribute("logined", false);
         else model.addAttribute("logined", true);
 
