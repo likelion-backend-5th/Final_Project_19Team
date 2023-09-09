457b6aae019510c4f2de6a07d3b336ffa68eaebc
moonwonki <moonwonki1999@gmail.com>
2023-09-09 14:30:08 +0900

    feat. 검색 관련 엔드포인트 수정
    - GET /main 에서 param으로 search 구별하도록 수정.

--- src/main/java/com/likelion/teammatch/controller/browser/MainController.java
+++ src/main/java/com/likelion/teammatch/controller/browser/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -30,20 +31,31 @@ public class MainController {
     @GetMapping("/main")
     public String getMainPage(
             @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+            @RequestParam(value = "search", required = false) String search,
             Model model
     ){
-        List<RecruitDraftDto> recruitInfoList = recruitService.getRecruitDraftList(page);
+        List<RecruitDraftDto> recruitInfoList;
+        if (search != null){
+            recruitInfoList = recruitService.getRecruitDraftListBySearch(page, search);
+        }
+        else {
+            recruitInfoList = recruitService.getRecruitDraftList(page);
+        }
+
         model.addAttribute("recruitList", recruitInfoList);
 
         // 프로필창 정보
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserProfileDto profileOfUser = userService.getProfileOfUser(username);
-        model.addAttribute("username", username);
-        model.addAttribute("phone", profileOfUser.getPhone());
-        model.addAttribute("email", profileOfUser.getEmail());
-        model.addAttribute("techStackList", profileOfUser.getTechStackList());
-        if (SecurityContextHolder.getContext().getAuthentication().getName().equals("anonymousUser")) model.addAttribute("logined", false);
-        else model.addAttribute("logined", true);
+
+        if (username.equals("anonymousUser")) model.addAttribute("logined", false);
+        else {
+            UserProfileDto profileOfUser = userService.getProfileOfUser(username);
+            model.addAttribute("username", username);
+            model.addAttribute("phone", profileOfUser.getPhone());
+            model.addAttribute("email", profileOfUser.getEmail());
+            model.addAttribute("techStackList", profileOfUser.getTechStackList());
+            model.addAttribute("logined", true);
+        }
 
         return "/html/index";
     }
