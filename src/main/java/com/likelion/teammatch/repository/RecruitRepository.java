51aa6d826be75a6475064e8fbe21b15e2725524a
moonwonki <moonwonki1999@gmail.com>
2023-09-09 14:26:05 +0900

    feat. 검색 구현을 위한 recruitRepository 메소드 추가
    - 정상 작동 확인함

--- src/main/java/com/likelion/teammatch/repository/RecruitRepository.java
+++ src/main/java/com/likelion/teammatch/repository/RecruitRepository.java
@@ -4,6 +4,8 @@ import com.likelion.teammatch.entity.Recruit;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ public interface RecruitRepository extends JpaRepository<Recruit, Long> {
     Page<Recruit> findAllByOrderByIdDesc(PageRequest pageReqeust);
 
     List<Recruit> findAllByTeamId(Long teamId);
+
+    @Query("SELECT e FROM Recruit e WHERE e.techStackWanted LIKE %:searchTerm% OR e.title LIKE %:searchTerm%")
+    Page<Recruit> searchByTechStackWantedOrTitle(PageRequest pageRequest, @Param("searchTerm") String searchTerm);
 }
