f2df82514208561ead9569e2bc94e3294a4b49de
moonwonki <moonwonki1999@gmail.com>
2023-09-10 19:25:24 +0900

    feat. exception 초안
    - GlobalExceptionHandler 추가

---
+++ src/main/java/com/likelion/teammatch/exception/status/Status400Exception.java
@@ -0,0 +1,7 @@
+package com.likelion.teammatch.exception.status;
+
+public class Status400Exception extends RuntimeException{
+    public Status400Exception(String message){
+        super(message);
+    }
+}
