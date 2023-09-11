13129471936ad813d23316e0198237277fdd5990
moonwonki <moonwonki1999@gmail.com>
2023-09-12 07:29:04 +0900

    feat. 팀 수정 관련 기능 구현
    - TeamTechStackRepository 메소드 추가
    - TeamService updateTeamInfo 메소드 수정
    - 엔드포인트 수정 및 추가 (GET /team/{teamId}/edit , POST /team/{teamId}/edit)
    
    Resolves: #54

--- src/main/java/com/likelion/teammatch/repository/TeamTechStackRepository.java
+++ src/main/java/com/likelion/teammatch/repository/TeamTechStackRepository.java
@@ -2,6 +2,7 @@ package com.likelion.teammatch.repository;
 
 
 import com.likelion.teammatch.entity.TeamTechStack;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ public interface TeamTechStackRepository extends JpaRepository<TeamTechStack, Lo
     List<TeamTechStack> findAllByTeamId(Long teamId);
 
     List<TeamTechStack> findAllByTechStackId(Long teamId);
+
+    @Transactional
+    void deleteByTeamId(Long teamId);
 }
