f213d61e9627c39a97161b06230286a41c02b1f7
Leesiyun <livre303@gmail.com>
2023-09-12 11:26:08 +0900

    feat: 팀원모집글 수정 화면에 기존 내용 표시-모집인원수 Related to: #57

--- src/main/java/com/likelion/teammatch/controller/browser/RecruitController.java
+++ src/main/java/com/likelion/teammatch/controller/browser/RecruitController.java
@@ -60,6 +60,7 @@ public class RecruitController {
         model.addAttribute("teamName", dto.getTeamName());
         model.addAttribute("teamRecruitName", dto.getRecruitTitle());
         model.addAttribute("teamRecruitDetails", dto.getTeamRecruitDetails());
+        model.addAttribute("memberNum", dto.getRecruitMemberNum());
 
         return "html/create_recruit";
     }
